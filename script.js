e733e789d4e0106429f1295bed0c67308a29a2bc
unknown <sfasfa@.com>
2022-03-30 11:01:32 +0500

    fixed many things

--- script.js
+++ script.js
@@ -1,60 +1,56 @@
 let arrows = document.querySelectorAll('.clocks-arrow');
 const btnStart = document.querySelector('.btn-start');
-let seconds = document.querySelector('.seconds'),
-        minutes = document.querySelector('.minutes'),
-        hours = document.querySelector('.hours');
-let i = 0;
+let mlseconds = document.querySelector('.mlseconds'),
+    seconds = document.querySelector('.seconds'),
+    minutes = document.querySelector('.minutes'),
+    hours = document.querySelector('.hours');
+let index = 0;
+let id;
+let stopArrow;
 btnStart.addEventListener('click', function(e){
     e.preventDefault();
-    if(this.innerHTML == 'start'){
-        this.innerHTML = 'stop';
-        setTimeout(stopwatchActive, 1000, this);
+    if(this.textContent == 'start'){
+        this.textContent = 'stop';
+        stopwatch();
     }
-    else if(this.innerHTML == 'stop'){
-        this.innerHTML = 'clear';
+    else if(this.textContent == 'stop'){
+        this.textContent = 'clear';
+        clearTimeout(id);
+        clearTimeout(stopArrow);
     }
-    else{
-        this.innerHTML = 'start';
-        seconds.innerHTML = 0;
-        minutes.innerHTML = 0;
-        hours.innerHTML = 0;
-        for( let i of arrows){
-            i.classList.remove('active');
-            arrows[arrows.length - 1].classList.add('active');
-        }
+    else if(this.textContent == 'clear'){
+        this.textContent = 'start';
+        mlseconds.textContent = 0;
+        seconds.textContent = 0;
+        minutes.textContent = 0;
+        hours.textContent = 0;
+        arrowActive();
     }
 })
 
-function stopwatchActive(btn){
-    arrowActive();
-    if(btnStart.innerHTML == 'stop'){
-        if(seconds.innerHTML < 59){
-            seconds.innerHTML++;
-        }
-        else if(seconds.innerHTML == 59){
-            seconds.innerHTML = 0;
-            minutes.innerHTML++;
-        }
-        if(seconds.innerHTML == 59 && minutes.innerHTML == 59){
-            seconds.innerHTML = 0;
-            minutes.innerHTML = 0;
-            hours.innerHTML++;
-        }
-        setTimeout(stopwatchActive, 1000);
+function stopwatch(){
+    if(mlseconds.textContent < 10) mlseconds.textContent++;
+    if(mlseconds.textContent == 10){
+        mlseconds.textContent = 0;
+        seconds.textContent++;
     }
-    
+    if(seconds.textContent == 60){
+        seconds.textContent = 0;
+        minutes.textContent++;
+    }
+    if(minutes.textContent == 60){
+        minutes.textContent = 0;
+        hours.textContent++;
+    }
+    stopArrow = setTimeout(arrowActive, 1000, seconds.textContent);
+    id = setTimeout(stopwatch, 100); 
 }
-function arrowActive(){
+function arrowActive(index){
+    index = index ? index : arrows.length - 1;
     for (let i = 0; i < arrows.length; i++){
         arrows[i].classList.remove('active');
     }
-    
-    if (i < arrows.length){
-        arrows[i].classList.add('active');
-        i++;
-    }
-    else if(i == arrows.length) i = 0;
-    if(btnStart.innerHTML == 'clear') i = 0;
+    arrows[index].classList.add('active');
 }
 
 
